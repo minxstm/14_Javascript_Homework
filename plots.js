5d5462988e7e17024980fc35ee53b51b4daf8ccf
Minh Huynh <minhqhuynh@gmail.com>
2018-05-15 21:08:14 -0700

    add index and plots

---
+++ plots.js
@@ -0,0 +1,6 @@
+var tbody = document.querySelector("tbody");
+var dateTimeInput = document.querySelector("dateTime");
+var searchButton = = document.querySelector("search");
+
+searchbutton.addEventListener("click",handleSearchButtonClick);
+
